087e8a3ba81de16124d5be54ee7b6f5a80c9e66e
cacho <394369786@qq.com>
2018-01-21 15:07:50 +0800

    add BeautifulGirls

--- getEachBeauty.py
+++ getEachBeauty.py
@@ -20,6 +20,8 @@ while(id <= 759):
         #通过参数id控制美女，参数p控制第几页
         url = requests.get('http://www.xingmeng365.com/articles.asp?id='+str(id)+'&mm='+str(p), headers=headers)
         print("当前爬取的网址为："+url.url)
+        #如果网页设置了charset=gb2312，那么需要设置编码方式为gb2312，否则乱码
+        url.encoding = 'gb2312'
         html_doc = url.text
         # 此处用url不带".text"的话报错，Python: object of type 'Response' has no len()
         # 错误解决：https://stackoverflow.com/questions/36709165/python-object-of-type-response-has-no-len
@@ -34,11 +36,22 @@ while(id <= 759):
                     image_url = "http://www.xingmeng365.com/" + image_url[6:]
                 else:
                     image_url = "http://www.xingmeng365.com/" + image_url[1:]   # id=7以后，[6:]改为[1:]
-                if not os.path.exists('photos'):
-                    os.makedirs('photos')
+                #获取h1标题
+                FileName = soup.find('h1').get_text()
+                # 获取标题的内容
+               # FileName = soup.title.get_text()
+                #切换到文件相对路径，在此路径创建一个新目录
+                #os.chdir('D:\BeautifulGirls')
+                #放到其他路径
+                os.chdir('F:\Lingwei')
+                image_path = 'AllPhoto/' + str(FileName)
+                if not os.path.exists(image_path):
+                    os.makedirs(image_path)
+                #切换到新的目录，图片才能存入到新创建的目录下
+                os.chdir(image_path)
                 print("开始下载第"+str(num)+"张图片："+image_url)
                 #可以自定义命名图片名称，好检索
-                file = open('photos/'+str(id)+'.'+str(p)+'-'+str(num)+'.jpg',"wb")
+                file = open(str(id)+'.'+str(p)+'-'+str(num)+'.jpg',"wb")
                 req = urllib.request.Request(url=image_url, headers=headers)
                 try:
                     image = urllib.request.urlopen(req, timeout=20)
