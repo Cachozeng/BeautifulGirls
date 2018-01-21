17ee79f467feda9efd1f9b19e1fa10320f6b5e35
cacho <394369786@qq.com>
2018-01-21 16:12:10 +0800

    add BeautifulGirls

--- Submit.bat
+++ Submit.bat
@@ -7,12 +7,9 @@ git init
 git remote add gitee https://gitee.com/Cacho/BeautifulGirls.git
 git remote add github https://github.com/Cachozeng/BeautifulGirls.git
 
-git pull gitee master
-git pull github master
-
 git add .
 git commit -m "add BeautifulGirls"
 git push -f gitee master
-git push github master
+git push -f github master
 
 pause
